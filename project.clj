b516022dc9515fd2366e71a8923b5e26c566d72c
Lars Gregori <lars.gregori@gmail.com>
2010-04-30 23:37:15 +0200

    updated to newer clojure 1.2 version: seq-contains? instead of includes?; changed version to 0.3.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject compojure-gae "0.3.3"
+(defproject compojure-gae "0.3.4-SNAPSHOT"
   :description "A concise web framework for Clojure and google app engine"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [commons-codec "1.3"]
                  [commons-io "1.4"]
                  [org.mortbay.jetty/jetty "6.1.21"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0"]]
   :namespaces [compojure
                compojure.control
                compojure.html
